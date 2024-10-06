52475e55386a6dd18a0b565968947fe006708323
NeroBoyka <russelldossouyovo@gmail.com>
2024-10-06 23:59:54 +0200

    8- Test pour main 100%

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,20 @@
 from olympics.__main__ import main
-
+import pytest
+import argparse
 
 def test_countries():
     argv = ['countries']
     main(argv)
+
+def test_collective():
+    argv = ['collective']
+    main(argv)
+
+def test_individual():
+    argv = ['individual']
+    main(argv)
+
+def test_negative_top_2():
+    with pytest.raises(argparse.ArgumentTypeError):
+        argv = ['individual', '--top', '-5']
+        main(argv)
